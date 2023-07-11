d69ad896c7e60a16e4dde9f0f89578c2afe721c4
shubham3246 <raj2013atel.gmail.com>
2023-07-11 20:55:26 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import SubHeading from './SubHeading/SubHeading.jsx';
+import Newsletter from './Footer/Newsletter.jsx';
+import FooterOverlay from './Footer/FooterOverlay.jsx';
+import MenuItem from './Menuitem/MenuItem.jsx';
+import Navbar from './Navbar/Navbar.jsx';
+
+export {
+  SubHeading,
+  Newsletter,
+  FooterOverlay,
+  MenuItem,
+  Navbar,
+};
---
+++ src/container/index.js
@@ -0,0 +1,22 @@
+import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
+import FindUs from './FindUs/FindUs';
+import Footer from './Footer/Footer.jsx';
+import Gallery from './Gallery/Gallery.jsx';
+import Header from './Header/Header.jsx';
+import Intro from './Intro/Intro.jsx';
+import Laurels from './Laurels/Laurels.jsx';
+import SpecialMenu from './Menu/SpecialMenu.jsx';
+// import FindUs from './FindUs/FindUs';
+
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+};
